8bafd3c972b35dc6ad3ff71ee2fb2da047451d1f
Alan Tavares Morais <pollargithub@gmail.com>
2024-08-05 16:30:12 -0300

    adicionando funcionalidade troca de background

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, watch } from "vue";
 import { storeToRefs } from "pinia";
 import useAudioStore from "@renderer/stores/useAudioStore";
 import sound from "@renderer/assets/audios/sound.mp3";
 import soundStart from "@renderer/assets/audios/sound2.mp3";
 import directoryPath from "@renderer/stores/useDirectoryPath";
+import useLanguageStore from "./stores/useLanguageStore";
+import bg_default from "@renderer/assets/images/bg.png";
+import bg_english from "@renderer/assets/images/bg_en.png";
 
+const lang = useLanguageStore();
 const store = useAudioStore();
 const directory = directoryPath();
 const { audio, audioStart } = storeToRefs(store);
+const { language } = storeToRefs(lang);
+
+const body:HTMLElement = document.querySelector("body") as HTMLElement;
 
 onMounted(() => {
   directory.verifyDirectoryExist();
+  alternateBg();
 })
+
+watch(() => language.value, () => {
+  alternateBg();
+})
+
+function alternateBg(){
+  const bg = language.value === "default" ? bg_default : bg_english;
+  body.style.backgroundImage = `url(${bg})`;
+}
 
 </script>
 
